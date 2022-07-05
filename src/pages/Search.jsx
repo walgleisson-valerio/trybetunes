c3226cfc82d39c074492d694992a17ec883ccb83
walgleisson valerio <walgleissonvaleriodev@gmail.com>
2022-07-05 17:33:56 -0300

    Resolve requisito 05

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,12 +2,52 @@ import React from 'react';
 import Header from '../Components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isButtonDisable: true,
+      text: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    this.setState(
+      { [target.name]: target.value },
+      () => this.handleButton(),
+    );
+  }
+
+  handleButton() {
+    const { text } = this.state;
+    if (text.length >= 2) {
+      this.setState(
+        { isButtonDisable: false },
+      );
+    }
+  }
+
   render() {
+    const { isButtonDisable, text } = this.state;
     return (
       <>
         <Header />
         <div data-testid="page-search">
-          <h1>Search</h1>
+          <input
+            type="search"
+            data-testid="search-artist-input"
+            name="text"
+            value={ text }
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ isButtonDisable }
+            onClick={ this.onClick }
+          >
+            Pesquisar
+          </button>
         </div>
       </>
     );
