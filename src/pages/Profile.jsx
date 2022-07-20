6404ec76ff6726000c6a3e793713f0de0e1daf05
walgleisson valerio <walgleissonvaleriodev@gmail.com>
2022-07-20 20:17:10 -0300

    Resolve resquisto 13

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,52 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isLoading: true,
+      user: {},
+    };
+  }
+
+  componentDidMount = async () => {
+    const user = await getUser();
+    this.setState({
+      user,
+      isLoading: false,
+    });
+  }
+
   render() {
+    const {
+      isLoading,
+      user: { name, email, image, description },
+    } = this.state;
     return (
       <>
         <Header />
         <div data-testid="page-profile">
-          <h1>Proofile</h1>
+          {isLoading ? <Loading /> : (
+            <>
+              <h1>Profile</h1>
+              <h3>{ name }</h3>
+              <p>{ email }</p>
+              <img
+                src={ image }
+                alt={ name }
+                data-testid="profile-image"
+              />
+              <p>{ description }</p>
+              <Link to="/profile/edit">
+                Editar perfil
+              </Link>
+            </>
+          )}
         </div>
       </>
     );
